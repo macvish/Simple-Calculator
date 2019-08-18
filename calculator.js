a51fe8dbc9edab01addc0f78cc2ac0ff35063b18
Macanthony Ahaotu <macintony1@gmail.com>
2019-08-18 17:26:58 +0100

    7th Commit

---
+++ calculator.js
@@ -0,0 +1,37 @@
+const one = () => {
+    const btnone = document.querySelector('#row1').value;
+    document.querySelector('input').value = btnone;
+}
+
+const two = () => {}
+
+const three = () => {}
+
+const four = () => {}
+
+const five = () => {}
+
+const six = () => {}
+
+const seven = () => {}
+
+const eight = () => {}
+
+const nine = () => {}
+
+const dot = () => {}
+
+const zero = () => {}
+
+const add = () => {}
+
+const sub = () => {}
+
+const mult = () => {}
+
+const div = () => {}
+
+const equ = () => {
+    let field = document.querySelector('input').value;
+    console.log(field);
+}
